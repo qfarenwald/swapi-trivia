23e7b98b22b18f2a9e1ec826a0a51fe6d53459d9
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-08 17:05:38 -0600

    Add test for updateuserstate function

--- src/App.test.js
+++ src/App.test.js
@@ -12,10 +12,23 @@ describe('App', () => {
   });
 
   it('should match the snapshot with correct data', () => {
-    const wrapper = shallow(
-      <App />
-    )
+    const wrapper = shallow(<App />);
+
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should update user state with data from form', () => {
+    const wrapper = shallow(<App />);
+
+    const mockUserObj = {
+      name: 'Mike',
+      quote: 'May the force be with you',
+      status: 'Jedi'
+    }
+
+    wrapper.instance().updateUserState(mockUserObj);
+
+    expect(wrapper.state('user')).toEqual(mockUserObj);
+  })
 
 })
