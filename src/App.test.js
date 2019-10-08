3e00ccf995964845eb10cd246b8439eceafeeab5
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-08 16:53:22 -0600

    Add app file and test structure with snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should match the snapshot with correct data', () => {
+    const wrapper = shallow(
+      <App />
+    )
+    expect(wrapper).toMatchSnapshot();
+  });
+
+})
