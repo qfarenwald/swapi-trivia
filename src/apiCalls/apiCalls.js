90a57436086d1f93f9662b652b55c93903a800d7
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-10 16:24:58 -0600

    Add character property and value to movie data

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -4,10 +4,12 @@ export const getMovies = (moviesData) => {
       .then(films => {
         return films.results.map((film) => {
           const newDate = film.release_date.split('-')[0]
+          const tenCharacters = film.characters.slice(0, 10)
           return {
             title: film.title,
             episode_id: film.episode_id,
-            release_date: newDate
+            release_date: newDate,
+            characters: tenCharacters
           }
         })
       })
@@ -16,5 +18,5 @@ export const getMovies = (moviesData) => {
 export const getCharacters = (characData) => {
   return fetch(characData)
     .then(res => res.json())
-    .then(characters => console.log(characters))
+    // .then(characters => console.log(characters))
 }
