a80cb0a2a5511cfc42e92d58422fbab29f640da6
Michael Schneider <michaelschneider@Michaels-MBP.PK5001Z>
2019-10-11 15:04:49 -0600

    Update folders and files and allow clicking on movie to update current characters

---
+++ src/Components/CharactersContainer/CharactersContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CharactersContainer.css';
+
+const CharactersContainer = () => {
+  return (
+    <section>
+      <p>Characters Container</p>
+    </section>
+  )
+}
+
+export default CharactersContainer;
