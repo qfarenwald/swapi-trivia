ab0518837fd6977584e7dede322489b2f54f2e1e
Michael Schneider <michaelschneider@Michaels-MBP.PK5001Z>
2019-10-11 16:22:05 -0600

    Allow use to click on a movie s characters and be directed to a new URL showing those characters

---
+++ src/Components/Characters/Characters.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Characters.css'
+
+const Characters = (props) => {
+  return (
+    <section>
+      <p>{props.name}</p>
+    </section>
+  )
+}
+
+export default Characters;
--- src/Components/CharactersContainer/CharactersContainer.js
+++ src/Components/CharactersContainer/CharactersContainer.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import './CharactersContainer.css';
+import Characters from '../Characters/Characters'
 
 const CharactersContainer = (props) => {
   console.log("these are CharactersContainer  props::", props)
+  let characters = props.characters.map((character, index) => {
+    console.log(character)
+    return (<Characters
+      key={index}
+      name={character.name}
+      homeworld={character.homeworld}
+      species={character.species}
+      films={character.films}
+    />
+    )
+  })
+
   return (
     <section>
-      <p>Characters Container</p>
+      {characters}
     </section>
   )
 }
