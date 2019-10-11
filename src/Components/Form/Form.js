9396e85505bfd2e4bebe7e72afd95fbd38109f11
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-10 21:21:36 -0600

    Change status input to radio buttons

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -33,6 +33,7 @@ class Form extends Component {
       <main className="form">
         <p style={{visibility: this.state.isValid ? 'hidden' : 'visible'}}>Please enter values for each input!</p>
         <input
+          className="input-fill"
           type="text"
           placeholder="Name"
           name="name"
@@ -40,18 +41,27 @@ class Form extends Component {
           onChange={event => this.updateFormState(event, 'name')}
         />
         <input
+          className="input-fill"
           type="text"
           placeholder="Favorite Quote"
           name="quote"
           value={this.state.quote}
           onChange={event => this.updateFormState(event, 'quote')}
         />
-        <input
-          type="radio"
-          name="status"
-          value={this.state.status}
-          onChange={event => this.updateFormState(event, 'status')}
-        />
+        <div className="input-container">
+          <label className="input-radio">
+            <input type="radio" name="status" value="PADAWAN" onChange={event => this.updateFormState(event, 'status')}/> PADAWAN
+            <span class="checkmark"></span>
+          </label>
+          <label className="input-radio">
+            <input type="radio" name="status" value="JEDI" onChange={event => this.updateFormState(event, 'status')}/> JEDI
+            <span class="checkmark"></span>
+          </label>
+          <label className="input-radio">
+            <input type="radio" name="status" value="YODA" onChange={event => this.updateFormState(event, 'status')}/> YODA
+            <span class="checkmark"></span>
+          </label>
+        </div>
         <Link to={this.state.isValid ? "/movies" : "/"}><button>Submit</button></Link>
       </main>
     )
