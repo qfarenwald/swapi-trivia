9a5c7b4dfab72a9fbb15cf2cf8548bf928b649fe
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-12 15:12:14 -0600

    Fetch all character data and add to movie state

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -16,7 +16,47 @@ export const getMovies = (moviesData) => {
       })
   }
 
-// export const getCharacters = (characData) => {
-//   return fetch(characData)
-//     .then(res => res.json())
-// }
+export const getCharacterData = (characterURL) => {
+  return fetch(characterURL)
+    .then(res => res.json())
+    .then(character => {
+      const { name, homeworld, species, films } = character;
+      return fecthAllCharacterData(homeworld, species, films)
+        .then(res => ({name, res}))
+    })
+}
+
+const fecthAllCharacterData = (homeworldURL, speciesURL, filmsURL) => {
+  const homeworld = getHomeworld(homeworldURL)
+  const species = getSpecies(speciesURL)
+  const films = getFilms(filmsURL)
+  return Promise.all( [homeworld, species, films] )
+}
+
+const getHomeworld = (homeworldURL) => {
+  return fetch(homeworldURL)
+    .then(res => res.json())
+    .then(homeworld => {
+      return ({
+        home: homeworld.name,
+        population: homeworld.population
+      })
+    })
+}
+
+const getSpecies = (speciesURL) => {
+  return fetch(...speciesURL)
+    .then(res => res.json())
+    .then(species => {
+      return species.name
+    })
+  }
+
+const getFilms = (filmsURL) => {
+  const films = filmsURL.map((film) => {
+    return fetch(film)
+      .then(res => res.json())
+      .then(film => film.title)
+  })
+  return Promise.all(films)
+}
