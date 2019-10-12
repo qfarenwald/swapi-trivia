af9ae3d3ea19e2b7bc9bcad1d2eb450165268945
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-12 17:35:19 -0600

    Style character card and characterscontainer

--- src/Components/Characters/Characters.js
+++ src/Components/Characters/Characters.js
@@ -3,8 +3,12 @@ import './Characters.css'
 
 const Characters = (props) => {
   return (
-    <section>
+    <section className="charac-card">
       <p>{props.name}</p>
+      <p>{props.species}</p>
+      <p>{props.homeworld}</p>
+      <p>{props.population}</p>
+      <p>{props.films}</p>
     </section>
   )
 }
