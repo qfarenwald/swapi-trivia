2b03b3fbec4023137d0d89cc8e2e501a33792c1e
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-12 21:01:54 -0600

    Add menu as newly designed to charactercontainer component

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -6,10 +6,10 @@ const Menu = ( props ) => {
 
   return (
     <nav className="menu">
-      <Route exact path='/movies' render={() => <h1 className='small-heading'><span className='yellow-text'>SW</span>API</h1>} />
+      <Route path='/movies' render={() => <h1 className='small-heading'><span className='yellow-text'>SW</span>API</h1>} />
       <div className="favandlog">
-        <Route exact path='/movies' render={() => <p>FAVORITES</p>} />
-        <Route exact path='/movies' render={() => <Link className="link" to='/'><p onClick={() => props.removeUserState()}>LOGOUT</p></Link>} />
+        <Route path='/movies' render={() => <p>FAVORITES</p>} />
+        <Route path='/movies' render={() => <Link className="link" to='/'><p onClick={() => props.removeUserState()}>LOGOUT</p></Link>} />
       </div>
     </nav>
   )
