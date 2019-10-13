4e6b64c937d469216d2a6f40b7a1bd8027e8c64b
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-12 21:09:42 -0600

    Edit menu spacing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -49,6 +49,7 @@ class App extends Component {
         <Route exact path='/' render={() => <Form updateUserState={this.updateUserState}/>} />
         <Route path='/movies' render={() => <Profile name={this.state.user.name} quote={this.state.user.quote} status={this.state.user.status}/>} />
         <Route exact path='/movies' render={() => <h2>EPISODES</h2>} />
+        <Route exact path='/movies/:id' render={() => <h2>CHARACTERS</h2>} />
         <Route exact path='/movies' render={() => <MoviesContainer movies={this.state.movies} user={this.state.user} updateCurrentCharacters={this.updateCurrentCharacters}/>} />
         <Route exact path='/movies/:episode_id' render={() => {
           return (
