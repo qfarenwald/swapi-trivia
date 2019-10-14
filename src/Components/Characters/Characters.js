dce6494f408f3460db6c6558230d9e0367f19e01
Michael Schneider <michaelschneider@Michaels-MacBook-Pro.local>
2019-10-14 15:12:13 -0600

    Make absolutely sure all characters are loaded before user can proceed

--- src/Components/Characters/Characters.js
+++ src/Components/Characters/Characters.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Characters.css'
+import { isProperty } from '@babel/types';
 
 const Characters = (props) => {
   return (
     <section className="charac-card">
-      <div className="favorite" onClick={() => props.updateFavoriteCharacters(props)}>
+      <div className="favorite" onClick={() => {
+        props.updateFavoriteCharacters(props)
+        props.isFavorited ? props.isFavorited = false : props.isFavorited = true
+        }}>
       </div>
       <h3>{props.name}</h3>
       <h4><span className="bold-text">SPECIES </span>{props.species}</h4>
