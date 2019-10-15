8bd3acf2687bdccdec73d73a665a03ad35ea7be3
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-15 16:51:32 -0600

    Updates

---
+++ precache-manifest.0591d1f61e29a5aadebf06723b86bba0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8101cde089756ff523dcda5993c1b41",
+    "url": "./index.html"
+  },
+  {
+    "revision": "425b61fee6ef2697f3a1",
+    "url": "./static/css/main.524f1247.chunk.css"
+  },
+  {
+    "revision": "7623a55abeeb3bfd004d",
+    "url": "./static/js/2.34048092.chunk.js"
+  },
+  {
+    "revision": "425b61fee6ef2697f3a1",
+    "url": "./static/js/main.1133c374.chunk.js"
+  },
+  {
+    "revision": "38f612cba680545d3472",
+    "url": "./static/js/runtime-main.201c1743.js"
+  },
+  {
+    "revision": "e1382a4ad46749c0b0551114f4f706dc",
+    "url": "./static/media/swapi-stars.e1382a4a.svg"
+  },
+  {
+    "revision": "31bfd72e17287cb38dd3ee453a04ee73",
+    "url": "./static/media/swapi_heart_clicked.31bfd72e.svg"
+  },
+  {
+    "revision": "9e2f4c49decf4dbfabf71e7a76221ae8",
+    "url": "./static/media/swapi_heart_unclicked.9e2f4c49.svg"
+  }
+]);
